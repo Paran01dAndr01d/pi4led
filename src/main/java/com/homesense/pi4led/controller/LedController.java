28ad9f71f024ebc6952a740fa6a80f6f95591277
Paran01dAndr01d <tlhard@gmail.com>
2015-12-02 10:22:34 -0600

    commit 2

--- src/main/java/com/homesense/pi4led/controller/LedController.java
+++ src/main/java/com/homesense/pi4led/controller/LedController.java
@@ -1,9 +1,10 @@
 package com.homesense.pi4led.controller;
 
 import com.pi4j.io.gpio.*;
-import com.pi4j.wiringpi.Gpio;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class LedController {
 
     private static GpioPinDigitalOutput pin;
@@ -17,11 +18,17 @@ public class LedController {
     public String light() {
         if (pin == null) {
             GpioController gpio = GpioFactory.getInstance();
-            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "led1", PinState.LOW);
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
         }
 
+        pin.blink(100,1000);
+        pin.blink(200,1000);
+        pin.blink(500,1000);
+        pin.blink(1000,2000);
+        pin.blink(3000,3000);
         pin.toggle();
 
         return "OK";
     }
+
 }
